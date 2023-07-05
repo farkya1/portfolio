d6618224f82570ecc96aea9bf7df50561facbea6
farkya1 <farkasa1@msu.edu>
2023-07-04 23:06:18 -0400

    Messing with webgl build

--- reactapp/src/UnityGame.tsx
+++ reactapp/src/UnityGame.tsx
@@ -1,23 +1,55 @@
 
-import React,{Fragment,useEffect} from "react";
-import { Unity, useUnityContext } from 'react-unity-webgl';
+import { isVisible } from "@testing-library/user-event/dist/utils";
+import React,{Fragment,useState,useEffect} from "react";
+import { Unity, useUnityContext,} from 'react-unity-webgl';
 
 export default function UnityGame() {
-  const { unityProvider, requestFullscreen } = useUnityContext({
-    loaderUrl: "Unity/Build/temp.loader.js",
-    dataUrl: "Unity/Build/temp.data.unityweb",
-    frameworkUrl: "Unity/Build/temp.framework.js.unityweb",
-    codeUrl: "Unity/Build/temp.wasm.unityweb",
+const [fullscreen, setFullscreen] = useState(false)
+
+
+
+
+  const { unityProvider, requestFullscreen} = useUnityContext({
+    loaderUrl: "Unity/Build/Unity.loader.js",
+    dataUrl: "Unity/Build/Unity.data.unityweb",
+    frameworkUrl: "Unity/Build/Unity.framework.js.unityweb",
+    codeUrl: "Unity/Build/Unity.wasm.unityweb",
+    
   });
 
+  
+
+  
+  const [hidden,setHidden] = useState({display:"none"})
+
+  
+
+
   function handleClickEnterFullscreen() {
+    setHidden({display:"block"});
     requestFullscreen(true);
+
+    
+    
+    
+
   }
+
+  function handleKeyDown(event: KeyboardEvent) {
+    if (event.keyCode === 27 && hidden["display"]==="block") {
+      setHidden({display:"none"});
+      
+    }
+
+    
+  }
+  document.addEventListener('keydown', handleKeyDown);
+  
 
 
   return (
     <Fragment>
-      <Unity unityProvider={unityProvider} />
+      <Unity unityProvider={unityProvider} style={hidden}/>
       <button onClick={handleClickEnterFullscreen}>Enter Fullscreen</button>
     </Fragment>
   );
